d1fe1826bbf10ae065fd2e7344cf65c0884d2c32
Wei Liu <brentweiliu@yahoo-inc.com>
2016-12-31 01:18:19 -0800

    Find shortest path in a maze

--- findMinSteps.cpp
+++ findMinSteps.cpp
@@ -1,93 +1,103 @@
-/**
- * Find the shortest path from (0, 0) to exit (x, y) in 
- * a maze
- */
-#include <iostream>
-#include <vector>
-#include <queue>
-using namespace std;
-
-/**
- * Bread First Search
- * @param[in]   maze copy of input maze
- * @param[in]   rows, columns  size of maze
- * @param[in]   startX, startY start point of current BFS
- * @param[in]   exitX, exitY exit of maze
- * @param[out]  shortest shortest path steps number from start to exit
- */
-void BFS(vector<vector<int>> maze, int rows, int columns,
-         int startX, int startY,
-         int exitX, int exitY,
-         int *shortest)
-{
-    queue<pair<int, int>> q;
-    q.push(make_pair(-1, -1));
-    const int kMOVES = 4; //size of moves
-    //Trick 2: Use moves matrix to indicate move up, down, left, right
-    int moves[kMOVES][2] = {{0,1},{1,0},{0,-1},{-1,0}};
-    int level = -1;
-
-    if(maze[startX][startY]==1)   {
-        q.push(make_pair(startX, startY));
-        maze[startX][startY] = -1;
-    }
-
-    while(!q.empty()){
-        // current location <x, y> in maze
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-
-        //Trick 1: Use a “Sentinel: <-1,-1> ” to mark levels boudary.
-        if(x == -1 && y == -1) {
-            // Trick 1: terminate if queue contain <-1, -1> only
-            if(q.empty()) return;
-            ++level;
-            q.push(make_pair(-1, -1));
-            continue;
-        }
-        if(x == exitX && y == exitY) {
-            *shortest = level;
-            return;
-        }
-        for (int move = 0; move < kMOVES; ++move){
-            int ti = x + moves[move][0];
-            int tj = y + moves[move][1];
-            if (ti >= 0 && ti < rows && tj >= 0 && tj < columns &&
-                maze[ti][tj] == 1)
-            {
-                q.push(make_pair(ti, tj));
-            }
-        }
-        maze[x][y] = -1;
-    }
-}
-
-int findMinNumSteps(vector<vector<int>> &maze,
-                    int exitRow, int exitCol)
-{
-    int ret = -1;
-    int rows = maze.size();
-    if (rows <= 0) return -1;
-    int columns = maze[0].size();
-    BFS(maze, rows, columns, 0, 0, exitRow, exitCol, &ret);
-    return ret;
-}
-
-int main() {
-    vector<vector<int>> matrix = {
-            {1, 1, 1, 1, 1},
-            {1, 1, 0, 0, 0},
-            {0, 1, 1, 1, 0},
-            {1, 0, 0, 1, 0},
-            {1, 0, 0, 1, 0},
-            {1, 0, 1, 0, 0}
-    };
-    cout << findMinNumSteps(matrix, 4, 3) << endl; //7
-    cout << findMinNumSteps(matrix, 1, 1) << endl; //2
-    cout << findMinNumSteps(matrix, 0, 0) << endl; //0
-    cout << findMinNumSteps(matrix, 2, 0) << endl; //-1
-    cout << findMinNumSteps(matrix, 5, 2) << endl; //-1
-    return 0;
-}
-
+/** 
+ * Find the shortest path from (0, 0) to exit (x, y) in  a maze 
+ */  
+#include <iostream>  
+#include <vector>  
+#include <queue>  
+using namespace std;  
+using Maze = vector<vector<int>>;  
+  
+struct Cell {  
+    bool can_through;  
+    bool is_visited;  
+    int level;  
+    void init(bool x, bool y, int j){  
+        can_through = x;  
+        is_visited = y;  
+        level = j;  
+    }  
+};  
+/** 
+ * Traverse the graph with Breadth-First-Search 
+ * @param[in]   maze input maze 
+ * @param[in]   start_x, start_y start point of current BFS 
+ * @param[in]   exit_x, exit_y exit of maze 
+ * @return      shortest path steps number from start to exit 
+ */  
+int BreadFirstSearch(const Maze &maze,  
+                     const int start_x, const int start_y,  
+                     const int exit_x,  const int exit_y)  
+{  
+    const size_t rows    = maze.size();  
+    const size_t columns = maze[0].size();  
+    const int kMOVES = 4; //size of moves  
+    //Trick 2: Use moves matrix to indicate move up, down, left, right  
+    const int moves[kMOVES][2] = {{0,1},{1,0},{0,-1},{-1,0}};  
+    vector<vector<Cell>> maze_copy(rows, vector<Cell>(columns));  
+    for (int i = 0; i < rows; ++i){  
+        for (int j = 0; j < columns; ++j){  
+            maze_copy[i][j].init(maze[i][j] ? true : false, false, 0);  
+        }  
+    }  
+  
+    queue<pair<int, int>> q;  
+    if (maze_copy[start_x][start_y].can_through)   {  
+        q.push(make_pair(start_x, start_y));  
+        maze_copy[start_x][start_y].is_visited = true;  
+        maze_copy[start_x][start_y].level = 0;  
+    }  
+  
+    while(!q.empty()){  
+        // current location <x, y> in maze  
+        int x = q.front().first;  
+        int y = q.front().second;  
+        q.pop();  
+  
+        if (x == exit_x && y == exit_y) {  
+            return maze_copy[x][y].level;  
+        }  
+        for (int move = 0; move < kMOVES; ++move){  
+            int nx = x + moves[move][0];  
+            int ny = y + moves[move][1];  
+            if (nx >= 0 && nx < rows && ny >= 0 && ny < columns &&  
+                maze_copy[nx][ny].can_through &&  
+                !maze_copy[nx][ny].is_visited)  
+            {  
+                q.push(make_pair(nx, ny));  
+                maze_copy[nx][ny].level = maze_copy[x][y].level + 1;  
+                maze_copy[nx][ny].is_visited = true;  
+            }  
+        }  
+    }  
+    return -1;  
+}  
+  
+int FindMinNumSteps(const Maze &maze,  
+                    const int exit_x,  
+                    const int exit_y)  
+{  
+    if (maze.empty()) {  
+        return -1;  
+    }  
+    return BreadFirstSearch(maze, 0, 0, exit_x, exit_y);  
+}  
+  
+int main() {  
+    Maze maze1 = {  
+            {1, 1, 1, 1, 1},  
+            {1, 1, 0, 0, 0},  
+            {0, 1, 1, 1, 0},  
+            {1, 0, 1, 1, 0},  
+            {1, 0, 1, 1, 0},  
+            {1, 0, 1, 0, 0}  
+    };  
+    cout << FindMinNumSteps(maze1, 4, 3) << endl; //7  
+    cout << FindMinNumSteps(maze1, 1, 1) << endl; //2  
+    cout << FindMinNumSteps(maze1, 0, 0) << endl; //0  
+    cout << FindMinNumSteps(maze1, 2, 0) << endl; //-1  
+    cout << FindMinNumSteps(maze1, 5, 2) << endl; //7  
+    Maze maze2;  
+    cout << FindMinNumSteps(maze2, 5, 2) << endl; //-1  
+  
+    return 0;  
+}  
